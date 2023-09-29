22243141aa20f99a254d6d57cc6ab33d2b713aff
nategrift <nategrift@gmail.com>
2023-09-29 15:05:12 -0400

    add version check script

--- src/lib/Printer.ts
+++ src/lib/Printer.ts
@@ -50,3 +50,39 @@ export class Printer {
     console.log(`Found ${filePaths.length} files to generate tests for.`);
   }
 }
+
+// ANSI color escape sequences
+enum COLORS {
+  reset = '\x1b[0m',
+  red = '\x1b[31m',
+  yellow = '\x1b[33m',
+  blue = '\x1b[34m',
+  green = '\x1b[32m',
+  white = '\x1b[37m',
+}
+
+export class Color {
+  static colorize(text: string, color: COLORS) {
+    return color + text + COLORS.reset;
+  }
+
+  static red(text: string) {
+    return Color.colorize(text, COLORS.red);
+  }
+
+  static yellow(text: string) {
+    return Color.colorize(text, COLORS.yellow);
+  }
+
+  static blue(text: string) {
+    return Color.colorize(text, COLORS.blue);
+  }
+
+  static green(text: string) {
+    return Color.colorize(text, COLORS.green);
+  }
+
+  static white(text: string) {
+    return Color.colorize(text, COLORS.white);
+  }
+}
